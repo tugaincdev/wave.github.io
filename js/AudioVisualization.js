6610d7243d524d212facf7b8f10b3deb0ab36698
tugaincdev <tugaincdev@gmail.com>
2025-11-14 14:14:07 +0000

    Update AudioVisualization.js

--- js/AudioVisualization.js
+++ js/AudioVisualization.js
@@ -15,7 +15,7 @@ class AudioVisualization {
     this.testData = new Uint8Array(256);
     this.frameCount = 0;
 
-    // Inicializar dados de teste
+    // Inicializar dados de teste com padrão animado
     for (let i = 0; i < this.testData.length; i++) {
       this.testData[i] = Math.sin(i / 10) * 128 + 128;
     }
@@ -26,49 +26,93 @@ class AudioVisualization {
   }
 
   update() {
-    // TODO: atualizar estado da visualização
+    // Atualizar estado da visualização
     this.frameCount++;
+
+    // dados de teste com animação APENAS se não houver audioProcessor
+    if (!this.audioProcessor) {
+      for (let i = 0; i < this.testData.length; i++) {
+        this.testData[i] = Math.sin(i / 10 + this.frameCount / 20) * 100 + 128;
+      }
+    }
   }
 
   resize(width, height) {
-    // TODO: redimensionar visualização
+    // Redimensionar visualização
     this.canvas.width = width;
     this.canvas.height = height;
   }
 
   getProperties() {
-    // TODO: obter propriedades da visualização
-    return this.properties;
+    // Obter propriedades da visualização
+    return { ...this.properties };
   }
 
   updateProperty(property, value) {
-    // TODO: atualizar propriedade
+    // Atualizar propriedade
     if (this.properties.hasOwnProperty(property)) {
       this.properties[property] = value;
+      return true;
     }
+    return false;
   }
 
   clearCanvas() {
-    // TODO: limpar canvas
+    // Limpar canvas
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
   }
 
   drawGrid() {
-    // TODO: desenhar grelha de fundo
+    // Desenhar grelha de fundo
+    const { width, height } = this.canvas;
+    const gridSize = 50;
+
+    this.ctx.strokeStyle = "rgba(255, 255, 255, 0.05)";
+    this.ctx.lineWidth = 1;
+
+    // Linhas verticais
+    for (let x = 0; x < width; x += gridSize) {
+      this.ctx.beginPath();
+      this.ctx.moveTo(x, 0);
+      this.ctx.lineTo(x, height);
+      this.ctx.stroke();
+    }
+
+    // Linhas horizontais
+    for (let y = 0; y < height; y += gridSize) {
+      this.ctx.beginPath();
+      this.ctx.moveTo(0, y);
+      this.ctx.lineTo(width, y);
+      this.ctx.stroke();
+    }
   }
 
   createGradient() {
-    // TODO: criar gradiente de cores
-    return this.ctx.createLinearGradient(
-      0,
-      0,
-      this.canvas.width,
-      this.canvas.height
-    );
+    //gradiente de cores BRILHANTES
+    const gradient = this.ctx.createLinearGradient(0, this.canvas.height, 0, 0);
+
+    gradient.addColorStop(0, "#00ffff"); // Ciano brilhante
+    gradient.addColorStop(0.33, "#ff00ff"); // Magenta brilhante
+    gradient.addColorStop(0.66, "#ffff00"); // Amarelo brilhante
+    gradient.addColorStop(1, "#ff0080"); // Rosa brilhante
+
+    return gradient;
   }
 
   normalizeData() {
-    // TODO: normalizar dados de áudio
-    return [];
+    
+    let data;
+
+    
+    if (this.audioProcessor && this.audioProcessor.getFrequencyData) {
+      data = this.audioProcessor.getFrequencyData();
+    } else {
+      data = this.testData;
+    }
+
+    //valores para range 0-1
+    const normalized = Array.from(data).map((value) => value / 255);
+
+    return normalized;
   }
 }
