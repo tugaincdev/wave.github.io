2096050fc286e78b3c4da84af0325c3f900ecf9c
tugaincdev <tugaincdev@gmail.com>
2025-11-14 14:14:57 +0000

    Update SpectrumVisualization.js

--- js/SpectrumVisualization.js
+++ js/SpectrumVisualization.js
@@ -1,31 +1,39 @@
-// Visualizações Concretas
 class SpectrumVisualization extends AudioVisualization {
-    constructor(canvas, audioProcessor) {
-        super(canvas, audioProcessor);
-        this.name = 'Espectro de Frequências';
-        // Inicializar propriedades específicas
+  constructor(canvas, audioProcessor) {
+    super(canvas, audioProcessor);
+    this.name = "Spectrum";
+    this.properties = {
+      barWidth: 4,
+      barSpacing: 2,
+      smoothing: 0.8,
+    };
+  }
+
+  draw() {
+    this.clearCanvas();
+    const data = this.normalizeData();
+    const { width, height } = this.canvas;
+    const { barWidth, barSpacing } = this.properties;
+
+    const totalBarWidth = barWidth + barSpacing;
+    const numBars = Math.floor(width / totalBarWidth);
+
+    const gradient = this.createGradient();
+
+    // Add glow effect
+    this.ctx.shadowBlur = 20;
+    this.ctx.shadowColor = "#ff00ff";
+
+    for (let i = 0; i < numBars && i < data.length; i++) {
+      //data aimplified
+      const barHeight = Math.pow(data[i], 0.7) * height * 0.9;
+      const x = i * totalBarWidth;
+      const y = height - barHeight;
+
+      this.ctx.fillStyle = gradient;
+      this.ctx.fillRect(x, y, barWidth, barHeight);
     }
-    
-    draw() {
-        // TODO: desenhar espectro de frequências
-        this.clearCanvas();
-        
-        // Implementação básica para teste
-        const data = this.audioProcessor ? this.audioProcessor.getFrequencyData() : this.testData;
-        const barWidth = this.canvas.width / data.length;
-        
-        for (let i = 0; i < data.length; i++) {
-            const barHeight = (data[i] / 255) * this.canvas.height;
-            const x = i * barWidth;
-            const y = this.canvas.height - barHeight;
-            
-            this.ctx.fillStyle = `hsl(${i / data.length * 360}, 100%, 50%)`;
-            this.ctx.fillRect(x, y, barWidth - 1, barHeight);
-        }
-    }
-    
-    getProperties() {
-        // TODO: obter propriedades específicas
-        return super.getProperties();
-    }
+
+    this.ctx.shadowBlur = 0;
+  }
 }
