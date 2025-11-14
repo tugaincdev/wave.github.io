e04b9968304a244d9ff04ca3734e9ed96e3bc84e
tugaincdev <tugaincdev@gmail.com>
2025-11-14 14:17:21 +0000

    Update WaveformVisualization.js

--- js/WaveformVisualization.js
+++ js/WaveformVisualization.js
@@ -1,41 +1,57 @@
-
 class WaveformVisualization extends AudioVisualization {
-    constructor(canvas, audioProcessor) {
-        super(canvas, audioProcessor);
-        this.name = 'Forma de Onda';
-        // Inicializar propriedades específicas
-    }
+  constructor(canvas, audioProcessor) {
+    super(canvas, audioProcessor);
+    this.name = "Waveform";
+    this.properties = {
+      lineWidth: 3,
+      amplitude: 1.0,
+    };
+  }
+
+  normalizeData() {
     
-    draw() {
-        // TODO: desenhar forma de onda
-        this.clearCanvas();
-        
-        // Implementação básica para teste
-        const data = this.audioProcessor ? this.audioProcessor.getWaveformData() : this.testData;
-        const sliceWidth = this.canvas.width / data.length;
-        
-        this.ctx.beginPath();
-        this.ctx.moveTo(0, this.canvas.height / 2);
-        
-        for (let i = 0; i < data.length; i++) {
-            const v = data[i] / 128.0;
-            const y = v * this.canvas.height / 2;
-            const x = i * sliceWidth;
-            
-            if (i === 0) {
-                this.ctx.moveTo(x, y);
-            } else {
-                this.ctx.lineTo(x, y);
-            }
-        }
-        
-        this.ctx.strokeStyle = '#4cc9f0';
-        this.ctx.lineWidth = 2;
-        this.ctx.stroke();
+    let data;
+
+    if (this.audioProcessor && this.audioProcessor.getWaveformData) {
+      data = this.audioProcessor.getWaveformData();
+    } else {
+      data = this.testData;
     }
-    
-    getProperties() {
-        // TODO: obter propriedades específicas
-        return super.getProperties();
+
+    return Array.from(data).map((value) => value / 255);
+  }
+
+  draw() {
+    this.clearCanvas();
+    const data = this.normalizeData();
+    const { width, height } = this.canvas;
+    const { lineWidth, amplitude } = this.properties;
+
+    //glow effect
+    this.ctx.shadowBlur = 15;
+    this.ctx.shadowColor = "#00ffff";
+    this.ctx.strokeStyle = this.createGradient();
+    this.ctx.lineWidth = lineWidth;
+    this.ctx.lineCap = "round";
+    this.ctx.lineJoin = "round";
+    this.ctx.beginPath();
+
+    const sliceWidth = width / data.length;
+
+    for (let i = 0; i < data.length; i++) {
+      const x = i * sliceWidth;
+      // Amplify waveform
+      const v = (data[i] - 0.5) * amplitude * 1.5;
+      const y = height / 2 + (v * height) / 2;
+
+      if (i === 0) {
+        this.ctx.moveTo(x, y);
+      } else {
+        this.ctx.lineTo(x, y);
+      }
     }
+
+    this.ctx.stroke();
+    this.ctx.shadowBlur = 0;
+  }
 }
